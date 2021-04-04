6559bb51d2c515c26c458797d944f672727c8fb5
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-04-04 12:03:36 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+var num1 = 31
+var num2 = 2
+var num3 = 20
+var num4 = 5
 
-// Write your code here
+const multiply = num1 * num2
+
+const random = parseInt(Math.random() * 10);
+
+const mod = num3/num4
+
+const max = Math.max(1, 2, 3, 4, 20);
